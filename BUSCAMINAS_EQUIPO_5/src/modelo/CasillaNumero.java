ff2bbf073c042fae4c92504cdebe58c01316b5a3
ikasle <ikasle@DAM1-20.arangoya.local>
2025-04-04 13:57:32 +0200

    Version: 0.0.6

--- BUSCAMINAS_EQUIPO_5/src/modelo/CasillaNumero.java
+++ BUSCAMINAS_EQUIPO_5/src/modelo/CasillaNumero.java
@@ -9,4 +9,14 @@ public class CasillaNumero extends Casilla {
         this.numero = numero;
     }
 
+	public int getNumero() {
+		return numero;
+	}
+
+	
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+
 }
