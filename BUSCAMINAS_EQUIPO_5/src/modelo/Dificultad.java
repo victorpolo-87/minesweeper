7ecb9a014074dc8a49a4626411516beb1dbd4840
ikasle <ikasle@DAM1-14.arangoya.local>
2025-04-04 13:08:57 +0200

    0.0.5

--- BUSCAMINAS_EQUIPO_5/src/modelo/Dificultad.java
+++ BUSCAMINAS_EQUIPO_5/src/modelo/Dificultad.java
@@ -2,42 +2,44 @@ package modelo;
 
 public class Dificultad {
 
-	private DificultadEnum dificultadEnum;
-	private int tamanioFilas;
-	private int tamanioColumnas;
+    private DificultadEnum dificultadEnum;
+    private int tamanioFilas;
+    private int tamanioColumnas;
+    private int cantidadMinas;
 
-	public Dificultad(DificultadEnum dificultadEnum, int tamanioFilas, int tamanioColumnas) {
-		this.dificultadEnum = dificultadEnum;
-		this.tamanioFilas = tamanioFilas;
-		this.tamanioColumnas = tamanioColumnas;
-	}
+    public Dificultad(DificultadEnum dificultadEnum) {
+        this.dificultadEnum = dificultadEnum;
+        this.tamanioFilas = dificultadEnum.getFilas();
+        this.tamanioColumnas = dificultadEnum.getColumnas();
+        this.cantidadMinas = dificultadEnum.getMinas();
+    }
 
-	
-	public DificultadEnum getDificultadEnum() {
-		return dificultadEnum;
-	}
+    public DificultadEnum getDificultadEnum() {
+        return dificultadEnum;
+    }
 
+    public void setDificultadEnum(DificultadEnum dificultadEnum) {
+        this.dificultadEnum = dificultadEnum;
+        this.tamanioFilas = dificultadEnum.getFilas();
+        this.tamanioColumnas = dificultadEnum.getColumnas();
+        this.cantidadMinas = dificultadEnum.getMinas();
+    }
 
-	public void setDificultadEnum(DificultadEnum dificultadEnum) {
-		this.dificultadEnum = dificultadEnum;
-	}
+    public int getTamanioFilas() {
+        return tamanioFilas;
+    }
 
-	public int getTamanioFilas() {
-		return tamanioFilas;
-	}
+    public int getTamanioColumnas() {
+        return tamanioColumnas;
+    }
 
-	
-	public void setTamanioFilas(int tamanioFilas) {
-		this.tamanioFilas = tamanioFilas;
-	}
+    public int getCantidadMinas() {
+        return cantidadMinas;
+    }
 
-	public int getTamanioColumnas() {
-		return tamanioColumnas;
-	}
-
-	
-	public void setTamanioColumnas(int tamanioColumnas) {
-		this.tamanioColumnas = tamanioColumnas;
-	}
-
+    @Override
+    public String toString() {
+        return dificultadEnum.toString();
+    }
 }
+
--- BUSCAMINAS_EQUIPO_5/src/modelo/DificultadEnum.java
+++ BUSCAMINAS_EQUIPO_5/src/modelo/DificultadEnum.java
@@ -1,7 +1,35 @@
 package modelo;
 
 public enum DificultadEnum {
-facil,
-normal,
-dificil
+    FACIL(8, 8, 10),
+    NORMAL(16, 16, 40),
+    DIFICIL(24, 24, 99);
+
+    private final int filas;
+    private final int columnas;
+    private final int minas;
+
+    DificultadEnum(int filas, int columnas, int minas) {
+        this.filas = filas;
+        this.columnas = columnas;
+        this.minas = minas;
+    }
+
+    public int getFilas() {
+        return filas;
+    }
+
+    public int getColumnas() {
+        return columnas;
+    }
+
+    public int getMinas() {
+        return minas;
+    }
+
+    @Override
+    public String toString() {
+        return name().charAt(0) + name().substring(1).toLowerCase()
+               + " (" + filas + "x" + columnas + ", " + minas + " minas)";
+    }
 }
